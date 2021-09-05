b7cd530f462bbf704364166816a3fe72ab512bbc
matt barbier <mdbarbier@protonmail.com>
2021-09-05 09:03:10 +0100

    Initial sln and proj creation

---
+++ promotionengine/promotionengine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace promotionengine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
