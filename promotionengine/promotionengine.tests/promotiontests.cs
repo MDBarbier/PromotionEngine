ed62a1c3ab02f3188dceabbfc165e790556e42dc
matt barbier <mdbarbier@protonmail.com>
2021-09-05 09:32:08 +0100

    Wrote failing tests for scenarios A,B,C

--- promotionengine/promotionengine.tests/promotiontests.cs
+++ promotionengine/promotionengine.tests/promotiontests.cs
@@ -1,39 +1,47 @@
 using System;
 using Xunit;
+using promotionengine.engine;
+using promotionengine.models;
 
 namespace promotionengine.tests
 {
     public class promotiontests
     {
-        [Fact]
-        public void ScenarioATest()
+        private List<Product> ProductList = new List<Product>();
+        private List<Promotion> PromotionList = new List<Promotion>();
+
+        [Theory]
+        [InlineData(1, 1, 1, 0, 100.00)]        
+        [InlineData(5, 5, 1, 0, 370.00)]
+        [InlineData(3, 5, 1, 1, 280.00)]
+        public void ScenarioTest(int amountSkuA, int amountSkuB, int amountSkuC, int amountSkuD, float expectedTotalPrice)
         {
             //Setup
-     
+            SetupTestData();
+            LoadProducts(ProductList);
+            LoadPromotions(PromotionList);
+            Order order = new Order() { new OrderItem() { Sku = 'A', Amount = amountSkuA }, new OrderItem() { Sku = 'B', Amount = amountSkuB },
+                new OrderItem() { Sku = 'C', Amount = amountSkuD }, new OrderItem() { Sku = 'D', Amount = amountSkuD }};            
+
             //Execution
+            OrderOutput orderOutput = OrderProcessor.ProcessOrder(order);
 
             //Assertion
+            Assert.True(orderOutput.TotalPrice == expectedTotalPrice, $"Test failure ({nameof(ScenarioATest)}): TotalPrice was {orderOutput.TotalPrice} but expected {expectedTotalPrice}");
         }
 
-        [Fact]
-        public void ScenarioBTest()
+        private void SetupTestData()
         {
-            //Setup
+            Product productA = new Product() { Sku = 'A', Price = 50.00 };
+            Product productB = new Product() { Sku = 'B', Price = 50.00 };
+            Product productC = new Product() { Sku = 'C', Price = 50.00 };
+            Product productD = new Product() { Sku = 'D', Price = 50.00 };
+            ProductList = new List<Product>() { productA, productB, productC, productD };
 
-            //Execution
-
-            //Assertion
+            Promotion promotion1 = new Promotion() { SingleSku = true, CombinedSku = false, ApplicableSkus = new char[] { 'A' }, NumUnitsRequired = 3, FixedPrice = 130.00, PercentageDiscount = 0.00 };
+            Promotion promotion2 = new Promotion() { SingleSku = true, CombinedSku = false, ApplicableSkus = new char[] { 'B' }, NumUnitsRequired = 2, FixedPrice = 45.00, PercentageDiscount = 0.00 };
+            Promotion promotion3 = new Promotion() { SingleSku = true, CombinedSku = false, ApplicableSkus = new char[] { 'C', 'D' }, NumUnitsRequired = 1, FixedPrice = 30.00, PercentageDiscount = 0.00 };
+            PromotionList = new List<Promotion>() { promotion1, promotion2, promotion3 };
         }
-
-        [Fact]
-        public void ScenarioCTest()
-        {
-            //Setup
-
-            //Execution
-
-            //Assertion
-        }
-
     }
 }
