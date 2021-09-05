c512f7470728dcf0ad8900f0cb7f26804ec4b726
matt barbier <mdbarbier@protonmail.com>
2021-09-05 09:17:06 +0100

    Add skeleton test outlines

---
+++ promotionengine/promotionengine.tests/promotiontests.cs
@@ -0,0 +1,39 @@
+using System;
+using Xunit;
+
+namespace promotionengine.tests
+{
+    public class promotiontests
+    {
+        [Fact]
+        public void ScenarioATest()
+        {
+            //Setup
+     
+            //Execution
+
+            //Assertion
+        }
+
+        [Fact]
+        public void ScenarioBTest()
+        {
+            //Setup
+
+            //Execution
+
+            //Assertion
+        }
+
+        [Fact]
+        public void ScenarioCTest()
+        {
+            //Setup
+
+            //Execution
+
+            //Assertion
+        }
+
+    }
+}
